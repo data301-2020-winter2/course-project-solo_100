1076a4c52a34047ed1a20805826fcd7539513def
Yash Atreya <yashatreya.ya@icloud.com>
2021-03-17 17:36:23 +0530

    Added Scatterplot - Score vs Comments

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,7 +1,9 @@
+from datetime import date
 import pandas as pd 
 import numpy as np 
 import seaborn as sns
 import matplotlib.pyplot as plt
+from datetime import date
 def load_and_process(url_or_csv: str):
     df1 = (
         pd.read_csv(url_or_csv)
@@ -19,5 +21,9 @@ def corr_matrix(df: pd.DataFrame):
     df1 = pd.DataFrame(data=df, columns=['score', 'num_of_comments', 'body_is_null'])
     cor = df1.corr()
     sns.heatmap(cor, annot=True)
+    plt.show()
+
+def score_comments(df: pd.DataFrame):
+    sns.scatterplot(data=df, x='num_of_comments', y='score', hue='body_is_null')
     plt.show()
     
