1d524b356b4b4b8c95d83de4e5fa43d0273ca467
Yash Atreya <yashatreya.ya@icloud.com>
2021-03-17 17:06:14 +0530

    Added Correlation Heatmap

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 def load_and_process(url_or_csv: str):
     df1 = (
         pd.read_csv(url_or_csv)
@@ -9,6 +10,14 @@ def load_and_process(url_or_csv: str):
     )
     df2 = (
         df1.sort_values(by=['timestamp','score'], ascending=True)
-        .to_csv('../data/processed/processed.csv')
+        .assign(body_is_null=lambda x: x['body'].isnull())
+        .reset_index(drop=True)
     )
     return df2
+
+def corr_matrix(df: pd.DataFrame):
+    df1 = pd.DataFrame(data=df, columns=['score', 'num_of_comments', 'body_is_null'])
+    cor = df1.corr()
+    sns.heatmap(cor, annot=True)
+    plt.show()
+    
