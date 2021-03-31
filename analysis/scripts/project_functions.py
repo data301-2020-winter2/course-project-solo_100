ea5299a550bd6680d28ebe24089126654509c442
Yash Atreya <yashatreya.ya@icloud.com>
2021-03-31 13:58:36 +0530

    Fixed #1

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,10 +1,10 @@
-from datetime import date
 import pandas as pd 
-import numpy as np 
+import numpy as np
+from pandas.core.frame import DataFrame 
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
-from datetime import date
+import re
 
 '''
 Milestone 2: Task - 3 is completed here
@@ -43,5 +43,28 @@ def against_timestamp(df: pd.DataFrame):
     # (optional) rotate by 90° the labels in order to improve their spacing
     plt.setp(ax.xaxis.get_majorticklabels(), rotation = 90)
     ax.xaxis.set_minor_locator(md.DayLocator(interval = 1))
+    plt.show()
+
+def pairplot(df: pd.DataFrame):
+    df.drop(columns=['timestamp'])
+    pairplot = sns.pairplot(df)
+    pairplot.fig.suptitle('All fields against each other', y=1.1)
+    plt.show()
+
+def boxplot(df: pd.DataFrame):
+    df.boxplot(column=['score', 'num_of_comments']).set_title('Score and Comments Distribution')
+    plt.show()
+
+def contains_gme(df: pd.DataFrame):
+    count = 0
+    t = df['title']
+    b = df['body']
+    for i in range(t.size):
+       if((t[i] != None and t[i] != np.NaN and re.search(r"gme", str(t[i]), flags=re.IGNORECASE | re.MULTILINE) != None) or (b[i] != None and b[i] != np.NaN and re.search(r"gme", str(b[i]), flags=re.IGNORECASE | re.MULTILINE) != None)):
+           count = count + 1
+    
+    pieDf = pd.DataFrame({'posts': [count, t.size - count]}, index=['$GME', '$OTHERS'])
+    pieDf.plot(kind='pie', y='posts')
+    plt.legend(loc='upper left')
     plt.show()
     
